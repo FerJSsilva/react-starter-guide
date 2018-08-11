3e7e8b6961ad63308dc47833069e54898e233ec1
Fernando Silva <development.ferjssilva@gmail.com>
2018-08-11 01:45:10 -0300

    feature: using a React Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './components/HelloWorld';
+import App from './components/App';
 
 import './assets/styles/style.sass';
 import './assets/styles/style.css';
 
 ReactDOM.render(
-  React.createElement(Hello),
+  React.createElement(App),
   document.getElementById('root'),
 );
 
