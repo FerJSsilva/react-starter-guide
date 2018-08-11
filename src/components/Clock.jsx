1f701d81526d29d45f9db0de7d0c55c5912bea97
Fernando Silva <development.ferjssilva@gmail.com>
2018-08-11 01:53:04 -0300

    feature: create a button to update Clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 class Clock extends React.Component {
   state = {
     date: new Date(),
+  }
+
+  updateClock = () => {
+    this.setState({
+      date: new Date(),
+    });
   }
 
   render() {
     return (
       <div>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.updateClock}>Update Clock</button>
       </div>
     );
   }
