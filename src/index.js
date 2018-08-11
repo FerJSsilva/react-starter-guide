8c4c2e953c7bfc807ce246209bfe2ff8dc712ae5
Fernando Silva <development.ferjssilva@gmail.com>
2018-08-11 01:43:46 -0300

    feature: a Hello World component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Hello from './components/HelloWorld';
+
+import './assets/styles/style.sass';
+import './assets/styles/style.css';
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
+  React.createElement(Hello),
+  document.getElementById('root'),
 );
 
+// Check if hot reloading is enable. If it is, changes won't reload the page.
+// This is related to webpack-dev-server and works on development only.
+if (module.hot) {
+  module.hot.accept();
+}
